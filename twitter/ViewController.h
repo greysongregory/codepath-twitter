04c7a18818820191bc22b37017290e2e196da3ab
Gregory Greyson <greyson.gregory@gmail.com>
2015-11-09 12:45:27 -0800

    Initial Commit

---
+++ twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  twitter
+//
+//  Created by Greyson Gregory on 11/9/15.
+//  Copyright © 2015 Grey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ twitter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  twitter
+//
+//  Created by Greyson Gregory on 11/9/15.
+//  Copyright © 2015 Grey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
